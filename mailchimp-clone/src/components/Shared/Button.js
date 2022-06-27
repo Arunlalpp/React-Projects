0d60189a9a020685166c43471f7c316230a8a0f6
arunlalpp <arunlalpp@gmail.com>
2022-06-28 02:13:16 +0530

    Feat:"Created resusable button"

---
+++ mailchimp-clone/src/components/Shared/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Button() {
+	return (
+		<>
+			<div className="bg-full">
+				<div
+					className="text-[#004e56] bg-transparent font-[600] py-[20px] px-[40px]  border-box border border-[#004e56] w-auto h-auto text-center"
+					style={{
+						fontFamily:
+							"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
+					}}
+				>
+					<a href="/">See More</a>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Button;
