9699fd59d4ba9bd27021cf2751dae27036922804
arunlalpp <arunlalpp@gmail.com>
2022-06-28 17:44:45 +0530

    Feat:"Added the container data in the mapped file"

--- mailchimp-clone/src/Images/Index.js
+++ mailchimp-clone/src/Images/Index.js
@@ -1,5 +1,15 @@
 import OutperformBanner1 from "../Images/OutperformBanner1.webp";
 import OutperformBanner2 from "../Images/OutperformBanner2.webp";
 import OutperformBanner3 from "../Images/OutperformBanner3.webp";
+import CustomerCompanyLogo1 from "../Images/CustomerCompanyLogo1.webp";
+import CustomerCompanyLogo2 from "../Images/CustomerCompanyLogo2.webp";
+import CustomerCompanyLogo3 from "../Images/CustomerCompanyLogo3.webp";
 
-export { OutperformBanner1, OutperformBanner2, OutperformBanner3 };
+export {
+	OutperformBanner1,
+	OutperformBanner2,
+	OutperformBanner3,
+	CustomerCompanyLogo1,
+	CustomerCompanyLogo2,
+	CustomerCompanyLogo3,
+};
