b484318df95854bd09e07ec30767c6f8ea495888
arunlalpp <arunlalpp@gmail.com>
2022-06-28 14:48:11 +0530

    Feat:"passed the button text as props"

--- mailchimp-clone/src/components/Shared/Button.js
+++ mailchimp-clone/src/components/Shared/Button.js
@@ -1,20 +1,16 @@
 import React from "react";
 
-function Button() {
+function Button({ Content }) {
 	return (
-		<>
-			<div className="bg-full">
-				<div
-					className="text-[#004e56] bg-transparent font-[600] py-[20px] px-[40px]  border-box border border-[#004e56] w-auto h-auto text-center"
-					style={{
-						fontFamily:
-							"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
-					}}
-				>
-					<a href="/">See More</a>
-				</div>
-			</div>
-		</>
+		<button
+			className="text-[#004e56] bg-transparent font-[600] py-[20px] px-[40px]  border-box border border-[#004e56]  h-auto text-center"
+			style={{
+				fontFamily:
+					"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
+			}}
+		>
+			<a href="/">{Content}</a>
+		</button>
 	);
 }
 
