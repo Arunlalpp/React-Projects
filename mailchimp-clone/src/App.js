3be6e386bb24ec2cf95df636c29b50f0ff0849c1
arunlalpp <arunlalpp@gmail.com>
2022-06-28 19:41:04 +0530

    Feat:" created Plans banner with button "

--- mailchimp-clone/src/App.js
+++ mailchimp-clone/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import HomeBanner from "./components/HomeBanner";
 import Inspirations from "./components/Inspirations";
 import OutperformContainer from "./components/OutperformContainer";
+import PlansBanner from "./components/PlansBanner";
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 			<OutperformContainer />
 			<CustomerExperience />
 			<Inspirations />
+			<PlansBanner />
 		</div>
 	);
 }
---
+++ mailchimp-clone/src/components/PlansBanner.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function PlansBanner() {
+	return (
+		<div className="bg-[#ffe01b]">
+			<div className="py-[5rem]">
+				<div className="w-full box-border max-w-[1600px] mx-auto px-[1.875rem] text-center">
+					<div className="mx-auto">
+						<h2
+							className="text-Primary text-[3.125rem] font-light leading-[1] tracking-[.0625rem]"
+							style={{
+								fontFamily: "Means Web,Georgia,Times,Times New Roman,serif",
+							}}
+						>
+							Let’s do this
+						</h2>
+					</div>
+					<div className="">
+						<div className="mt-3">
+							<p className="text-Primary text-[1.25rem] leading-[1.4] tracking-[0]">
+								Every big idea starts with a small step forward.
+							</p>
+						</div>
+						<div className="mt-[1.875rem] ">
+							<a
+								className="text-[#fff] bg-[#007c89] font-semibold text-[.9375rem] py-[20px] px-[40px] text-center cursor-pointer leading-[20.25px]"
+								href="/"
+								style={{
+									fontFamily:
+										"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
+								}}
+							>
+								{" "}
+								Pick A Plan{" "}
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default PlansBanner;
