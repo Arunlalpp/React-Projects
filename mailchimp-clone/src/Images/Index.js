cf282c5fb81fc9e31ca1be6b506c3ab9b16f8c53
arunlalpp <arunlalpp@gmail.com>
2022-06-28 18:56:12 +0530

    Feat:"Created the inspiration contaner"

--- mailchimp-clone/src/Images/Index.js
+++ mailchimp-clone/src/Images/Index.js
@@ -4,6 +4,9 @@ import OutperformBanner3 from "../Images/OutperformBanner3.webp";
 import CustomerCompanyLogo1 from "../Images/CustomerCompanyLogo1.webp";
 import CustomerCompanyLogo2 from "../Images/CustomerCompanyLogo2.webp";
 import CustomerCompanyLogo3 from "../Images/CustomerCompanyLogo3.webp";
+import Inspiration1 from "../Images/Inspiration1.webp";
+import Inspiration2 from "../Images/Inspiration2.webp";
+import Inspiration3 from "../Images/Inspiration3.webp";
 
 export {
 	OutperformBanner1,
@@ -12,4 +15,7 @@ export {
 	CustomerCompanyLogo1,
 	CustomerCompanyLogo2,
 	CustomerCompanyLogo3,
+	Inspiration1,
+	Inspiration2,
+	Inspiration3,
 };
