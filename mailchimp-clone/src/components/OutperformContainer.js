b205f2a57226f507b2453eb27e786be089a3f547
arunlalpp <arunlalpp@gmail.com>
2022-06-28 15:29:05 +0530

    Feat:"Container responsive foe mobile and desk "

--- mailchimp-clone/src/components/OutperformContainer.js
+++ mailchimp-clone/src/components/OutperformContainer.js
@@ -6,7 +6,7 @@ function OutperformContainer(props) {
 	return (
 		<div className="bg-white">
 			<div className="my-[6.25rem]">
-				<div className="w-full box-border max-w-[1600px] mx-auto px-[1.875rem] text-center">
+				<div className="w-full box-border max-w-[1600px] mx-auto px-[1.875rem] text-center flex flex-col ">
 					<h1
 						className="text-[2.5rem] leading-[1] tracking-[.3125rem] text-Primary font-light"
 						style={{
@@ -19,34 +19,36 @@ function OutperformContainer(props) {
 				</div>
 			</div>
 			<div className="bg-white">
-				<div className="w-full box-border max-w-[1600px] px-[1.875rem] mx-auto md:py-[3.75rem]">
+				<div className="w-full box-border max-w-[1600px] px-[1.875rem] mx-auto ">
 					{containerDatas.map((containerData) => {
 						return (
 							<>
-								<div className="">
-									<img src={containerData.url} alt="" />
-								</div>
-								<div className="mt-[60px]">
-									<h2
-										className="leading-[21.6px] text-[28px] text-Primary tracking-[0] font-light"
-										style={{
-											fontFamily:
-												"Means Web,Georgia,Times,Times New Roman,serif",
-										}}
-									>
-										{containerData.Title}
-									</h2>
-									<div
-										className="mt-[2.5rem] text-[16px] text-Secondary leading-[1.35]"
-										style={{
-											fontFamily:
-												"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
-										}}
-									>
-										{containerData.SubText}
+								<div className="lg:flex lg:flex-row lg:items-center lg:justify-center">
+									<div className="pt-[3.75rem] lg:w-[577.5px] lg:pr-[126.241px]">
+										<img src={containerData.url} alt="" />
 									</div>
-									<div className="mt-[2.5rem]">
-										<Button Content={containerData.Button} />
+									<div className="mt-[60px] lg:w-[577.5px] lg:pl-[57.7474px]">
+										<h2
+											className="leading-[21.6px] text-[28px] text-Primary tracking-[0] font-light"
+											style={{
+												fontFamily:
+													"Means Web,Georgia,Times,Times New Roman,serif",
+											}}
+										>
+											{containerData.Title}
+										</h2>
+										<div
+											className="mt-[2.5rem] text-[16px] text-Secondary leading-[1.35]"
+											style={{
+												fontFamily:
+													"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
+											}}
+										>
+											{containerData.SubText}
+										</div>
+										<div className="mt-[2.5rem]">
+											<Button Content={containerData.Button} />
+										</div>
 									</div>
 								</div>
 							</>
--- mailchimp-clone/src/components/Shared/Button.js
+++ mailchimp-clone/src/components/Shared/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ Content }) {
 	return (
 		<button
-			className="text-[#004e56] bg-transparent font-[600] py-[20px] px-[40px]  border-box border border-[#004e56]  h-auto text-center"
+			className="text-[#004e56] bg-transparent font-[600] py-[20px] px-[40px] box-border border border-[#56b044d8] hover:border-[#004e56] h-auto text-center"
 			style={{
 				fontFamily:
 					"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
