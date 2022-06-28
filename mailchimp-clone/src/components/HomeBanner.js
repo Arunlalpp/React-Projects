a37a18bf184c699c8561d46cf838acd3a9c65ab0
arunlalpp <arunlalpp@gmail.com>
2022-06-28 12:14:30 +0530

    Feat:"Home banner responsive completed"

--- mailchimp-clone/src/components/HomeBanner.js
+++ mailchimp-clone/src/components/HomeBanner.js
@@ -4,13 +4,13 @@ import Banner from "../Images/Banner.webp";
 function HomeBanner() {
 	return (
 		<div>
-			<div className="bg-full bg-[#ffe01b] py-[120px]">
-				<div className="box-border max-w-[100rem] mx-auto px-[1.875rem]">
-					<div className="mx-auto items-center flex">
-						<span className=" text-[#403b3b]">
-							<div className="w-ful">
+			<div className="bg-full bg-[#ffe01b] py-[120px] lg:pb-[50px] ">
+				<div className="box-border max-w-[1600px] mx-auto px-[1.875rem]">
+					<div className="mx-auto items-center lg:flex flex-row lg:w-[80%] md:w-[75%]">
+						<span className=" text-[#403b3b] lg:w-[50%] md:w-[75%]">
+							<div className="w-ful mt-[1.875rem]">
 								<h1
-									className="text-[2.5rem] tracking-[.03125rem] leading-[1] text-[#241c15] mt-[1.875rem] font-[450]"
+									className="text-[2.5rem] lg:text-[48px] tracking-[.03125rem] leading-[1] text-[#241c15] mt-[1.875rem] font-[450]"
 									style={{
 										fontFamily: "Means Web,Georgia,Times,Times New Roman,serif",
 									}}
@@ -29,34 +29,34 @@ function HomeBanner() {
 									automations from a marketing platform that has expert advice
 									built in.
 								</p>
-								<div className="flex flex-row items-center flex-wrap">
-									<div className="mt-[3rem] mr-[1.25rem]">
-										<a
-											href="/"
-											className="text-white bg-[#007c89] font-semibold text-[.9375rem] py-[20px] px-[40px] text-center cursor-pointer leading-[20.25px]"
-											style={{
-												fontFamily:
-													"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
-											}}
-										>
-											Sign Up
-										</a>
-									</div>
-									<div className="mt-[3rem] text-[#403b3b] border-none font-medium text-[.9375rem] py-0 ">
-										<a href="/">Compare Plans</a>
-									</div>
+							</div>
+							<div className="flex  flex-row mt-[3rem] mr-[1.25rem] items-center flex-wrap">
+								<div className="mr-[1.25rem]">
+									<a
+										href="/"
+										className="text-white bg-[#007c89] font-semibold text-[.9375rem] py-[20px] px-[40px] text-center cursor-pointer leading-[20.25px]"
+										style={{
+											fontFamily:
+												"Graphik Web,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif",
+										}}
+									>
+										Sign Up
+									</a>
 								</div>
-								<div className="pt-[4.9rem] w-[365.931px] h-[273.466px]">
-									<picture>
-										<img
-											className="block relative w-full h-auto"
-											src={Banner}
-											alt=""
-										/>
-									</picture>
+								<div className=" lg:mt-0 text-[#403b3b] border-none font-medium text-[.9375rem] py-0 ">
+									<a href="/">Compare Plans</a>
 								</div>
 							</div>
 						</span>
+						<div className="pt-[4.9rem] lg:pt-0 w-[365.931px] h-[273.466px] lg:w-[50%] lg:h-auto md:w-full md:h-auto">
+							<picture>
+								<img
+									className="block relative w-full h-auto"
+									src={Banner}
+									alt=""
+								/>
+							</picture>
+						</div>
 					</div>
 				</div>
 			</div>
