fd9a8a42c77db1dce440bde0e2af386abfb41bbd
arunlalpp <arunlalpp@gmail.com>
2022-06-30 16:20:44 +0530

    Feat:"Implemented the mobile view toggle"

--- mailchimp-clone/src/App.js
+++ mailchimp-clone/src/App.js
@@ -5,14 +5,15 @@ import Header from "./components/Header";
 import HomeBanner from "./components/HomeBanner";
 import Inspirations from "./components/Inspirations";
 import MailChimpFunctions from "./components/MailChimpFunctions";
+import MobileHeader from "./components/MobileHeader";
 import OutperformContainer from "./components/OutperformContainer";
 import PlansBanner from "./components/PlansBanner";
 
 function App() {
 	return (
 		<div>
+			<MobileHeader />
 			<Header />
-
 			<HomeBanner />
 			<OutperformContainer />
 			<CustomerExperience />
