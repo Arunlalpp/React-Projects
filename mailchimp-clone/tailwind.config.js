4c8eaf9ac17f8a11610f12273f82e8cd4885aa12
arunlalpp <arunlalpp@gmail.com>
2022-06-30 17:57:33 +0530

    Fix:"Color for the heading and subtext"

--- mailchimp-clone/tailwind.config.js
+++ mailchimp-clone/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		colors: {
-			Primary: "#241c15",
-			Secondary: "#403b3b",
+		extend: {
+			colors: {
+				Primary: "#241c15",
+				Secondary: "#403b3b",
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 };
