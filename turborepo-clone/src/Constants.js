08417894fa1f55f747ea7e42621931296edc3269
arunlalpp <arunlalpp@gmail.com>
2022-07-02 12:13:21 +0530

    Feat:"Container card data "

--- turborepo-clone/src/Constants.js
+++ turborepo-clone/src/Constants.js
@@ -92,55 +92,55 @@ const teamLogos = [
 
 const cardDatas = [
 	{
-		Icon: { RoundArrow },
+		Icon: <RoundArrow />,
 		Title: "Incremental builds",
 		SubText:
 			"Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
 	},
 	{
-		Icon: { Finger },
+		Icon: <Finger />,
 		Title: "Content-aware hashing",
 		SubText:
 			"Turborepo looks at the contents of your files, not timestamps to figure out what needs to be built.",
 	},
 	{
-		Icon: { Remote },
+		Icon: <Remote />,
 		Title: "Remote Caching",
 		SubText:
 			"Share a remote build cache with your teammates and CI/CD for even faster builds.",
 	},
 	{
-		Icon: { Parallel },
+		Icon: <Parallel />,
 		Title: "Parallel execution",
 		SubText:
 			"Execute builds using every core at maximum parallelism without wasting idle CPUs.",
 	},
 	{
-		Icon: { Zero },
+		Icon: <Zero />,
 		Title: "Zero runtime overhead",
 		SubText:
 			"Turborepo won’t interfere with your runtime code or touch your sourcemaps.",
 	},
 	{
-		Icon: { Pruned },
+		Icon: <Pruned />,
 		Title: "Pruned subsets",
 		SubText:
 			"Speed up PaaS deploys by generating a subset of your monorepo with only what's needed to build a specific target.",
 	},
 	{
-		Icon: { TaskPipeline },
+		Icon: <TaskPipeline />,
 		Title: "Task pipelines",
 		SubText:
 			"Define the relationships between your tasks and then let Turborepo optimize what to build and when.",
 	},
 	{
-		Icon: { MeetsYou },
+		Icon: <MeetsYou />,
 		Title: "Meets you where you’re at",
 		SubText:
 			"Using Lerna? Keep your package publishing workflow and use Turborepo to turbocharge task running.",
 	},
 	{
-		Icon: { Profile },
+		Icon: <Profile />,
 		Title: "Profile in your browser",
 		SubText:
 			"Generate build profiles and import them in Chrome or Edge to understand which tasks are taking the longest.",
