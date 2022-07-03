ae12921537176572ac969aa86d0d88de4560da86
arunlalpp <arunlalpp@gmail.com>
2022-07-03 23:41:36 +0530

    Feat:"changed the width and the disply alignments "

--- turborepo-clone/src/Components/Newsletter.js
+++ turborepo-clone/src/Components/Newsletter.js
@@ -19,13 +19,13 @@ function Newsletter() {
 					autocomplete="email"
 					required=""
 					value=""
-					class="w-full min-w-0 px-4 py-2 text-base text-gray-900 placeholder-gray-500 bg-gray-900 border border-transparent rounded-md appearance-none dark:text-white sm:text-sm dark:border-gray-700 dark:bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:border-white focus:placeholder-gray-400"
+					class="w-full min-w-0 px-4 py-2 text-base text-gray-900 placeholder-gray-500 bg-gray-900 border border-transparent rounded-md appearance-none dark:text-white sm:text-sm border-gray-700 bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-white focus:placeholder-gray-400"
 					placeholder="you@domain.com"
 				/>
 				<div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
 					<button
 						type="submit"
-						class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-black dark:bg-white dark:text-black border border-transparent rounded-md sm:text-sm betterhover:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-white dark:betterhover:hover:bg-gray-300"
+						class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-black  dark:text-black border border-transparent rounded-md sm:text-sm betterhover:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-white dark:betterhover:hover:bg-gray-300"
 					>
 						Subscribe
 					</button>
