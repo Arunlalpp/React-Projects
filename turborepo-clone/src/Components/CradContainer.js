705662116aeae502dbb5c7ff34fd513c0c258f34
arunlalpp <arunlalpp@gmail.com>
2022-07-03 23:33:59 +0530

    Feat:"Crad data svg updated"

--- turborepo-clone/src/Components/CradContainer.js
+++ turborepo-clone/src/Components/CradContainer.js
@@ -15,13 +15,15 @@ function CradContainer() {
 				<div class="grid grid-cols-1 mt-12 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
 					{cardDatas.map((cardData) => {
 						return (
-							<div class="p-10 bg-[#1d1d1d] shadow-lg rounded-xl dark:bg-opacity-5 ">
-								<div className="h-8 w-8 text-white">{cardData.Icon}</div>
+							<div class="p-10 bg-[#1d1d1d] shadow-lg rounded-xl bg-opacity-2 ">
+								<div className="h-8 w-8 text-white rounded-full p-1.5 bg-white dark:bg-opacity-1 bg-opacity-10 grid place-content-center">
+									{cardData.Icon}
+								</div>
 								<div class="mt-4">
 									<h3 class="text-lg font-medium text-white">
 										{cardData.Title}
 									</h3>
-									<p class="mt-2 text-base font-medium text-gray-500 dark:text-gray-400">
+									<p class="mt-6 text-base font-medium text-gray-500 dark:text-gray-400">
 										{cardData.SubText}
 									</p>
 								</div>
