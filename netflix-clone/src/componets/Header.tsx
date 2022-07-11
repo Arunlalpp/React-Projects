a9e7ed0f5fa7fa4157325f3214cb4c204c7a9f6c
arunlalpp <arunlalpp@gmail.com>
2022-07-12 01:02:44 +0530

    Feat:"created the header section for the mobile and desk"

---
+++ netflix-clone/src/componets/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import BannerContainer from "./BannerContainer";
+
+interface HeaderProps {
+	HeaderLogo: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
+export const Header = ({ HeaderLogo }: HeaderProps) => {
+	return (
+		<div className="Banner-image">
+			<div className="header">
+				<div className="pt-[0.5rem] Container">
+					<div className="">
+						<HeaderLogo className="wrapper" />
+					</div>
+					<div className="Container-selector">
+						<select placeholder="lang-switcher">
+							<option>Englsh</option>
+							<option>हिन्दी</option>
+						</select>
+						<div className="bg-[#e50914] px-[.5rem] py-[.25rem]">
+							<span className="text-white font-medium">Sign in</span>
+						</div>
+					</div>
+				</div>
+				<BannerContainer
+					BannerText="Unlimted movies,TV shows and more."
+					BannerSubText="Watch anywhere.Cancel anytime"
+					Bannertitle="Ready to watch? Enter your email to create or restart your membership."
+				/>
+			</div>
+		</div>
+	);
+};
