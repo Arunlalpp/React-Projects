0a4dac3f7f9eda89d5a897b36d8a76696b0012b5
arunlalpp <arunlalpp@gmail.com>
2022-07-19 03:49:04 +0530

    feat:" Created the children card container"

---
+++ netflix-clone/src/componets/ChildrenComponent.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ChildrenComponent() {
+	return (
+		<div>
+			<div className="md:px-[70px] px-[30px] py-[45px] mx-auto border-b-8 border-[#222]  border-t-8 bg-black">
+				<div className="max-w-[1100px] mx-auto flex md:flex-row justify-between items-center flex-col ">
+					<div className="text-white text-center">
+						<h1 className="text-[1.625rem] font-medium">
+							WCreate profiles for children.
+						</h1>
+						<h2 className="text-[1.125rem]">
+							Send children on adventures with their favourite characters in a
+							space made just for them—free with your membership.
+						</h2>
+					</div>
+					<div className="text-white">
+						<img
+							className=""
+							src="https://occ-0-4603-3663.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABYjXrxZKtrzxQRVQNn2aIByoomnlbXmJ-uBy7du8a5Si3xqIsgerTlwJZG1vMpqer2kvcILy0UJQnjfRUQ5cEr7gQlYqXfxUg7bz.png?r=420"
+							alt=""
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ChildrenComponent;
