eba58d97e44b55d3243005c93572f92f23e34af2
arunlalpp <arunlalpp@gmail.com>
2022-07-29 14:30:09 +0530

    feat: Add components router path

--- react-query/src/index.tsx
+++ react-query/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const queryclient =new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryclient}>
+      <BrowserRouter>
     <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
